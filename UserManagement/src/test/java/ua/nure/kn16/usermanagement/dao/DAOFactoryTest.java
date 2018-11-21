47992ebb31d23a2ac87fb46cf4ecc77888aac0e3
maryna.rybalko <maryna.rybalko@nure.ua>
2018-11-21 23:48:10 +0200

    DB was added

--- UserManagement/src/test/java/ua/nure/kn16/usermanagement/dao/DAOFactoryTest.java
+++ UserManagement/src/test/java/ua/nure/kn16/usermanagement/dao/DAOFactoryTest.java
@@ -1,5 +1,32 @@
 package ua.nure.kn16.usermanagement.dao;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import ua.nure.kn16.rybalko.usermanagement.dao.DAOFactory;
+import ua.nure.kn16.rybalko.usermanagement.dao.UserDAO;
+
+import static org.junit.Assert.*;
 
 public class DAOFactoryTest {
 
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void testGetUserDao() {
+        try{
+        DAOFactory daoFactory = DAOFactory.getInstance();
+        assertNotNull("Instance of DaoFactory is null", daoFactory);
+        UserDAO userDao = daoFactory.getUserDAO();
+        assertNotNull("Instance of UserDao is null", userDao);
+        }catch (RuntimeException e){
+            fail(e.toString());
+        }
+    }
 }
